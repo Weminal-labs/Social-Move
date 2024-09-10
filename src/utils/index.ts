ababb736a8ad4126cea6190a86914628fa504fc5
Alexandros De Great <126134422+pichtranst123@users.noreply.github.com>
2024-09-10 20:41:01 +0700

    testnet only

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,12 +2,10 @@ import { Aptos, AptosConfig, Network } from '@aptos-labs/ts-sdk';
 import { NetworkInfo } from '@aptos-labs/wallet-adapter-core';
 
 export const aptosClient = (network?: NetworkInfo | null) => {
-  if (network?.name === Network.DEVNET) {
-    return DEVNET_CLIENT;
-  } else if (network?.name === Network.TESTNET) {
+  if (network?.name === Network.TESTNET) {
     return TESTNET_CLIENT;
   } else if (network?.name === Network.MAINNET) {
-    throw new Error('Please use devnet or testnet for testing');
+    throw new Error('Please use testnet for testing');
   } else {
     const CUSTOM_CONFIG = new AptosConfig({
       network: Network.CUSTOM,
@@ -16,14 +14,6 @@ export const aptosClient = (network?: NetworkInfo | null) => {
     return new Aptos(CUSTOM_CONFIG);
   }
 };
-
-// Devnet client
-export const DEVNET_CONFIG = new AptosConfig({
-  network: Network.DEVNET,
-  fullnode: 'https://aptos.devnet.inola.movementlabs.xyz/v1',
-  faucet: 'https://faucet.devnet.inola.movementlabs.xyz',
-});
-export const DEVNET_CLIENT = new Aptos(DEVNET_CONFIG);
 
 // Testnet client
 export const TESTNET_CONFIG = new AptosConfig({ 
